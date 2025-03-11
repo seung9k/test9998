d8513d744d7f2e3a0c5b82d6320b4a710e308b27
KangSeungGu <aodnsrhf@naver.com>
2025-03-11 09:40:56 +0900

    250311 push

---
+++ src/main/java/sg/project_test1/ProjectTest1Application.java
@@ -0,0 +1,13 @@
+package sg.project_test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTest1Application.class, args);
+	}
+
+}
